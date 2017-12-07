07cc13ce762d1f0faa030d2af5c85fb33adee686
Mary Ruth Ngo <mngo@wellesley.edu>
2017-12-06 18:21:13 -0800

    exercise3 draft done

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -3,6 +3,12 @@ package com.thoughtworks.tw101.exercises.exercise3;
 public class OddSum {
 
     public int of(int start, int end) {
-        return -1;
+        int sumAllOddsToEnd = findSumAllOddsToN(end);
+        int sumAllOddsToStart = findSumAllOddsToN(start);
+        return sumAllOddsToEnd - sumAllOddsToStart;
+    }
+
+    private int findSumAllOddsToN(int n) {
+        return (int) Math.pow(((n+1)/2),2);
     }
 }
