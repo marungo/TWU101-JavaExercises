0828fd62be5831220ccf72c3403b0bbe04fe6e00
Mary Ruth Ngo <mngo@wellesley.edu>
2017-12-06 18:09:15 -0800

    exercise2 draft done. exercise1 draft updated

--- src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
@@ -5,19 +5,15 @@ package com.thoughtworks.tw101.exercises.exercise1;
 public class Main {
 
     private static void printOddNumbersAndTheirSum(int n) {
-        int i;
-        for (i = 0; i <= n; i++) {
-            if (i%2 == 1) {
-                System.out.print(i + " ");
-
-            }
-            if (i%10 == 0) {
-                System.out.println();
-            }
+        for (int i = 0; i <= n; i++) {
+            if (isOdd(i)) { System.out.print(i + " "); }
+            if (isTen(i)) { System.out.println(); }
         }
         printSumOddNumbers(n);
     }
 
+    private static boolean isOdd(int n) { return (n%2) == 1; }
+    private static boolean isTen(int n) { return (n%10) == 0; }
     private static void printSumOddNumbers(int n) {
         System.out.println("sum: " + Math.pow(((n+1)/2), 2));
     }
--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -1,4 +1,19 @@
 package com.thoughtworks.tw101.exercises.exercise2;
 
 public class Accumulator {
+    private int count;
+
+    public Accumulator() {
+        this.count = 0;
+    }
+
+    public void increment() {
+        count++;
+    }
+
+    public int total() {
+        System.out.println(count);
+        return count;
+    }
+
 }
