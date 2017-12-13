d63cb9fd366e2406a1deefb158aa7dd283e1406e
Mary Ruth Ngo <mngo@wellesley.edu>
2017-12-13 14:08:56 -0800

    fizzbuzz done

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/FizzBuzz.java
@@ -32,6 +32,16 @@ public class FizzBuzz {
     }
 
     private static void fizzBuzz() {
+        for (int i=1; i<=100;i++) {
 
+            if (i%3 == 0)
+                System.out.print("Fizz");
+            if (i%5 == 0)
+                System.out.print("Buzz");
+            if (!(i%3 == 0) && !(i%5 == 0))
+                System.out.print(i);
+
+            System.out.println();
+        }
     }
 }
