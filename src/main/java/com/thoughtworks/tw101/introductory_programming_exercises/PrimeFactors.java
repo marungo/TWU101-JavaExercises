377aee4ef6fdac6e03102f3682bd0cb7c005c770
Mary Ruth Ngo <mngo@wellesley.edu>
2017-12-13 15:08:50 -0800

    prime factors exercise done, JDK and language level updated to 1.8

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
@@ -7,13 +7,35 @@ package com.thoughtworks.tw101.introductory_programming_exercises;
 //  For example, generate(1) should return an empty list and generate(30) should return the numbers: 2,3,5.
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class PrimeFactors {
     public static void main(String[] args) {
         List<Integer> primeFactors = generate(30);
+        System.out.println(primeFactors.toString());
+    }
+
+    private static boolean isPrime(int n) {
+        return (findFactors(n).size() == 0);
+    }
+
+    private static List<Integer> findFactors(int n) {
+        List<Integer> potentialFactors = IntStream.rangeClosed(2, Math.abs(n)-1)
+                .boxed().collect(Collectors.toList());
+
+        List<Integer> factors = potentialFactors.stream()
+                .filter(f -> (n%f == 0))
+                .collect(Collectors.toList());
+
+        return factors;
     }
 
     private static List<Integer> generate(int n) {
-        return null;
+        List<Integer> primeFactors = findFactors(n).stream()
+                .filter(factor -> isPrime(factor))
+                .collect(Collectors.toList());
+        return primeFactors;
     }
+
 }
